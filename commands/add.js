0e1716cb80438d32e364ca59568a77854b67ca4d
yaseenagag <yaseen.uc@gmail.com>
2017-07-20 15:49:09 -0700

    fix add function

--- commands/add.js
+++ commands/add.js
@@ -1,11 +1,14 @@
-const { readTasks, writeTasks } = require('../task.js')
+const { readTasks, writeTasks } = require('../taskStore.js')
 
 const add = ( name, callBack, done=false  ) => {
   readTasks((error, todos) => {
     if (error) return callBack(error)
     let id 
     if (todos.legnth === 0 ) {
+      id = 1
+    } else {
       id = todos[todos.length-1].id+1
+      console.log('id++++++', todos[todos.length-1].id)
     }
     const newTodos = todos.concat({id, name, done})
     const newTodosJson = JSON.stringify(newTodos)
